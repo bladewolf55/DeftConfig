3b4a9c9776657e9321427fbd6d0e7457b80e102b
Charles Flatt <charles@tfhome.us>
2020-04-04 20:50:44 -0600

    Use Environment Variable for API

--- src/build/build-publish-remote.bat
+++ src/build/build-publish-remote.bat
@@ -3,6 +3,9 @@ rem add potential paths to PATH
 path=%path%;C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin
 path=%path%;C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin
 path=%path%;C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin
+path=%path%;C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin
+path=%path%;C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin
+path=%path%;C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin
 
 rem Build Initializer
 msbuild /property:Configuration=Release;OutDir=..\DeftConfig\content ..\DeftConfigInitializer\DeftConfigInitializer.csproj 
@@ -15,7 +18,7 @@ rem Delete everything in Package
 del Package\*.* /q
 
 rem Package
-nuget pack ..\DeftConfig\DeftConfig.nuspec -o Package
+nuget pack ..\DeftConfig\DeftConfig.nuspec -o Package -Prop Configuration=Release
 
 rem Publish
-nuget push Package\*.nupkg -Source https://api.nuget.org/v3/index.json
+nuget push Package\*.nupkg -Source https://api.nuget.org/v3/index.json -ApiKey %NuGetApi%
